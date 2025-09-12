2fd1bd3149ce6d594d6c448d95d2d139de402733
bubble <2933128731@qq.com>
2025-09-12 14:22:32 +0800

    Add HTML mail preview functionality and template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# spring-boot-course
+# spring-boot 课程的学习仓库
+
+## spring-boot-week1 快速开始模块
+
+### spring-boot-config 课程的学习仓库
--- spring-boot-config/src/main/java/top/jwxiang/boot/config/service/MailService.java
+++ spring-boot-config/src/main/java/top/jwxiang/boot/config/service/MailService.java
@@ -1,4 +1,9 @@
-package top.jwxiang.boot.config.service.impl;
+package top.jwxiang.boot.config.service;
+
+import top.jwxiang.boot.config.enums.ResultStatus;
+import top.jwxiang.boot.config.model.Mail;
 
 public interface MailService {
+    ResultStatus sendSimpleMail(Mail mail);
+    ResultStatus sendHtmlMail(Mail mail);
 }
