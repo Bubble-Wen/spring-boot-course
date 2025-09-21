6776455e96fa9cb11d692b9748da1053bec7dd9b
bubble <2933128731@qq.com>
2025-09-21 14:35:56 +0800

    feat: 提交异常处理模块代码（统一异常、业务异常定义）

---
+++ spring-boot-exception/src/main/java/top/jwxiang/ExceptionApplication.java
@@ -0,0 +1,10 @@
+package top.jwxiang;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ExceptionApplication.class, args);
+    }
+}
