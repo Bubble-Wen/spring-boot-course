b7eb69a05d401ff6bf40a1747603ea84c967bc31
bubble <2933128731@qq.com>
2025-10-11 08:07:49 +0800

    feat:SpringBoot定时任务（@Scheduled 与 Quartz）

---
+++ spring-boot-websocket/src/main/java/top/jwxiang/websocket/WebSocketApplication.java
@@ -0,0 +1,13 @@
+package top.jwxiang.websocket;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling // 启用定时任务
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
