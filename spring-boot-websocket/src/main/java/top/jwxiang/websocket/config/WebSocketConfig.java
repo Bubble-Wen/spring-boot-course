d615187df18927f93e198a39d3ac68a85b7f2e01
bubble <2933128731@qq.com>
2025-10-11 11:21:14 +0800

    Initial commit: spring-boot-websocket module

---
+++ spring-boot-websocket/src/main/java/top/jwxiang/websocket/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package top.jwxiang.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import top.jwxiang.websocket.handler.SimpleTimeWebSocketHandler;
+import top.jwxiang.websocket.handler.TestWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 测试的 WebSocket 端点
+        registry.addHandler(new TestWebSocketHandler(), "/ws/test").setAllowedOrigins("*");
+
+        registry.addHandler(new SimpleTimeWebSocketHandler(), "/ws/time").setAllowedOrigins("*");
+    }
+}
